8371647dba84f4bd1472df5d012c0b6de6a0ede4
saikiranmasoori133 <139746896+saikiranmasoori133@users.noreply.github.com>
2023-09-22 11:25:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,7 @@ streamlit.text(my_data_row)
 
 #import pandas
 streamlit.title('Masoori\'s Amazing Athleisure Catalog')
-# connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
